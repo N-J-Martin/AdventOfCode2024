4e97599da1d1a645f7dbf878b88d93544c6f0b51
Nadine Martin <nm275@st-andrews.ac.uk>
2024-12-20 15:42:24 +0000

    Day 9 Part 1 works for example, fails actual

---
+++ Day9/Day9Part1.java
@@ -0,0 +1,73 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day9Part1 {
+    private static String FILE = "Day9/Day9Sample.txt";
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(new File(FILE))) {
+            String line = sc.nextLine();
+            List<String> memory = RLEexpansion(line);
+            System.out.println(memory);
+            defragment(memory);
+            System.out.println(memory);
+            System.out.println(sum(memory));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<String> RLEexpansion(String encoding ) {
+        List<String> expanded = new ArrayList<>();
+        for (int i = 0; i < encoding.length() ; i++) {
+            int count = Integer.parseInt(String.valueOf(encoding.charAt(i)));
+            String[] miniArray = new String[count];
+            if (i % 2 == 0) {
+                /*
+                if (count == 0) {
+                    System.out.println("error?");
+                }
+                */
+                Arrays.fill(miniArray, String.valueOf(i/2));
+            } else {
+                Arrays.fill(miniArray, ".");
+            }
+            expanded.addAll(Arrays.asList(miniArray));
+        }
+        return expanded;
+    }
+
+    private static void defragment(List<String> space) {
+        int i = 0;
+        while (i < space.size()) {
+            if (space.get(i).equals(".")) {
+                space.remove(i);
+                int removal = space.size() - 1;
+                while (removal > i && space.get(removal).equals(".")) {
+                    removal--;
+                }
+                if (removal > i){
+                    space.add(i, space.get(removal));
+                    space.remove(removal+1);
+                }
+
+            }
+            i++;
+        }
+    }
+
+    private static int sum(List<String> space) {
+        int sum = 0;
+        for (int i = 0; i < space.size(); i++) {
+            if (!space.get(i).equals(".")){
+                sum = sum + (i * Integer.parseInt(space.get(i)));
+            }
+
+        }
+        return sum;
+    }
+
+}
